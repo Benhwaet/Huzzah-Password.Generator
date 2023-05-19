0cba19c438eacf70f913800494732c4429ebe170
BenHwaet <benmdesroches@gmail.com>
2023-05-19 18:22:54 -0300

    Restructured the files to contain all variable in a main function instead of having many self-contained functions (because of course). All variables determined within are then accessible even if they are local to the generatePassword function. Success and HUZZAHgit status... HOWEVER, the password textbox does not clear between password generations. Must add operation to clear box at every new button press.

--- assets/script.js
+++ assets/script.js
@@ -1,93 +1,70 @@
-// Assignment Code
+
 var generateBtn = document.querySelector("#generate");
+var password = document.querySelector("#password").value
+var passwordText = document.querySelector("#password")
+
 var characters = '';
+var numberChars = '';
 var lowercase = 'abcdefghijklmnopqrstuvwxyz';
 var uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 var numeric = '0123456789';
 var symbols = ' !#$%&*+,-./:;<=>?()"@\^_`{|}~';
-var numberChars;
-var password;
 
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  passwordText.value = password;
-}
-
-generateBtn.addEventListener("click", writePassword())
-
-function passwordLength() {
-    var numberChars = prompt("Please choose a number of characters for your password, between 8 and 128");
-    if (numberChars < 8 || numberChars > 128 || isNaN(numberChars)) {
-       alert("You must choose a number higher than 7 or lower than 129.");
-       passwordLength();
-    } else if (7 > numberChars < 129) {
-        alert("You have chosen a password length of " + numberChars + " characters");
-        return numberChars;
-    } else {
-        alert("Oops, that's not a number. Try again!");
-        passwordLength();
-        }
-    }      
-
-function addLowercase() {
-    // var lowercase = 'abcdefghijklmnopqrstuvwxyz';
-    var addLowercase = confirm("Would you like to include Lowercase characters to your password?");
-    if (addLowercase) {
-        characters += lowercase;   
-        } return characters;
-    }
-
-function addUppercase() {
-    // var uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    var addUppercase = confirm("Would you like to include Uppercase characters to your password?");    
-        if (addUppercase) {
-     characters += uppercase;
-     console.log(characters)
-        } return characters;
-    }
-
-function addNumeric() {
-    // var numeric = '0123456789';
-    var addNumeric = confirm("Would you like to include Numbers in your password?");
-    if (addNumeric) {
-        characters += numeric;
-        console.log(characters)
-        } return characters;  
-    }
-
-function addSymbols() {
-    // var symbols = ' !#$%&*+,-./:;<=>?()"@\^_`{|}~';
-    var addSymbols = confirm("Would you like to add Special Characters to your password?");
-    if (addSymbols) {
-        characters += symbols;
-        console.log(characters)
-        } return characters;
-    }
-
-    
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+    passwordText.value = password;
+  }
 
 function generatePassword() {
-    passwordLength()
-    addLowercase();
-    addUppercase();
-    addNumeric();
-    addSymbols();
-
-    console.log(characters)
-
-    if (characters.length < 1) {
-        alert("You need to choose at least one set of characters, please try again.")
-        generatePassword();
-    } else {   
-       var chars = characters.slice('');
+        var numberChars = (prompt("Please choose a number of characters for your password, between 8 and 128"));
+        
+            if (numberChars < 8 || numberChars > 128 || isNaN(numberChars)) {
+                alert("You must choose a number higher than 7 or lower than 129.");
+                generatePassword()
+                } else if (7 > numberChars < 129) {
+                    alert("You have chosen a password length of " + numberChars + " characters");
+                } else {
+                alert("Oops, try again!");
+                generatePassword();
+                }
+      
+        var addLowercase = confirm("Would you like to include -lowercase- characters to your password?");
+            if (addLowercase) {
+                characters += lowercase;   
+                }
+            
+        var addUppercase = confirm("Would you like to include -uppercase- characters to your password?");    
+            if (addUppercase) {
+                 characters += uppercase;
+                 console.log(characters)
+                }
+                
+        var addNumeric = confirm("Would you like to include -numbers- in your password?");
+            if (addNumeric) {
+                characters += numeric;
+                }  
+        
+        var addSymbols = confirm("Would you like to add -special characters/symbols- to your password?");
+            if (addSymbols) {
+                characters += symbols;
+                }
+                        
+    
+            if (characters.length < 1) {
+                alert("You need to choose at least one set of characters, please try again.")
+                generatePassword();
+            } else if (characters.length > 1) {
        
-       console.log(chars)
-
-       for (var i = 0; i <+ numberChars; i++){
-       passwordText = Math.floor(Math.random() * numberChars.length);
-       password += characters.substring(passwordText, passwordText+1);
-       document.querySelector("#password").value = password;
-           }       
-        }}
-     
+        //      for (var i = 0; i <= numberChars; i ++) {
+        // var random = Math.floor(Math.random() * characters.length);
+        //         passwordText += characters.slice('');
+                for (var i = 0; i <= numberChars; i++) {
+                    var randomNumber = Math.floor(Math.random() * characters.length);
+                    password += characters.substring(randomNumber, randomNumber +1);
+                   }
+            console.log(password)
+     }      return password;
+    }   
+    
+    generateBtn.addEventListener("click", writePassword);
